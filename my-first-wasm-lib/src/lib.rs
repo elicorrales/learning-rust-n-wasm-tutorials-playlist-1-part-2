9f5872f43f5903c76e7e4c62198745d8881818ed
Eli Corrales <e_corrales2003@yahoo.com>
2022-08-31 22:58:25 -0400

    .

--- my-first-wasm-lib/src/lib.rs
+++ my-first-wasm-lib/src/lib.rs
@@ -2,3 +2,23 @@
 pub fn say_hello() {
     println!("Hello World!");
 }
+
+#[no_mangle]
+pub fn say_hello_2() -> String {
+    "Hello World!".to_string()
+}
+
+#[no_mangle]
+pub fn echo(message: String) -> String {
+    message
+}
+
+#[no_mangle]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[no_mangle]
+pub fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
--- nodejs.wasm.runner/runner.js
+++ nodejs.wasm.runner/runner.js
@@ -6,9 +6,48 @@ if (process.argv.length < 3) {
 }
 pathToWasm = process.argv[2];
 
+let funcName;
+if (process.argv.length > 3) {
+    funcName = process.argv[3];
+}
+
+let varA;
+let varB;
+if (process.argv.length > 4) { varA = process.argv[4]; }
+if (process.argv.length > 5) { varB = process.argv[5]; }
+
 const fs = require('fs');
 const wasmBuffer = fs.readFileSync(pathToWasm);
 
-WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
-    console.log(wasmModule.instance.exports);
-});
+WebAssembly.instantiate(wasmBuffer)
+    .then(wasmModule => {
+
+        console.log('WASM Module:');
+        console.log(wasmModule);
+
+        console.log('WASM Module Exports:');
+        console.log(wasmModule.instance.exports);
+
+        if (funcName) {
+            console.log('WASM Module Function:');
+            const func = wasmModule.instance.exports[funcName];
+            console.log('Running function ', funcName, ' no params.');
+            const result = func();
+            console.log(result);
+            if (varA) {
+                console.log('Running function ', funcName, ' params varA.');
+                const result2 = func(varA);
+                console.log(result2);
+            }
+            if (varB) {
+                console.log('Running function ', funcName, ' params varA, varB.');
+                const result3 = func(varA, varB);
+                console.log(result3);
+            }
+        }
+    })
+    .catch(error => {
+        console.log('\n\nThere was an error:\n\n');
+        console.log(error);
+        console.log('\n\nI dont think it likes handling WASM32-WASI, just WASM32-unknown-unknown.\n\n');
+    });
